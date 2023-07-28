27b9cd693f2cae3344c833e922e10f8dffd52476
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-07-28 10:41:47 +0300

    crudCollab

---
+++ backend/routes/crud.js
@@ -0,0 +1,6 @@
+const User = require('../Modele/User');
+const Departement = require('../Modele/Departement');
+const Poste = require('../Modele/Poste');
+const router = require('express').Router();
+
+router.post('/addUser')
