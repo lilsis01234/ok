8e122a703b56862de776c795d93b50d35fed2f66
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-01 17:27:16 +0300

    liste des collaborateurs

--- frontend/src/components/Administrateur/CrudCollab/listeCollab.jsx
+++ frontend/src/components/Administrateur/CrudCollab/listeCollab.jsx
@@ -7,18 +7,45 @@ import { Link } from "react-router-dom";
 
 const ListeCollab = () => {
 
+  const[recherche,setRecherche]=useState('null');
   const [Collabs, setCollabs] = useState([]);
+  // const[Poste,setPoste]=useState([])
+ if(recherche===''){
+  setRecherche('null')
+ }
   useEffect(()=> {
     axios.get('http://localhost:4000/api/collaborateur/all_collaborateurs')
     .then(res => setCollabs(res.data))
     .catch(err => console.log(err));
   },[])  
 
-
   return (
     <div className="bodyliste">
+  <div className='flex'>
       <button className="ajout"><Link to='/admin/add'>+</Link></button>
+      <button className="ajout2"><Link to='/admin/postes'>Les postes</Link></button>
       <h1>Liste des Collaborateurs</h1>
+
+      <input type='text' id='recherche'className="add-input" placeholder="rechercher ici" onChange={(e)=>setRecherche(e.target.value)}/>
+  </div>
+    <table className="listeCollabo">
+      {Collabs.filter(collab=>collab.nom.includes(recherche)).map(Collab=>(
+       <tr key={Collab.id}>
+         <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+         <td>{Collab.matricule}</td>
+         <td>{Collab.nom}</td>
+         <td>{Collab.prenom}</td>
+         <td>{Collab.lot}</td>
+         <td>{Collab.tel}</td>
+         <td>{Collab.dateNaissance}</td>
+         <td>{Collab.titrePoste}</td>
+         <td>{Collab.departement}</td>
+         <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+         </tr>
+      ))}
+    </table>
+
+
       <table className="listeCollabo">
         <thead>
         <tr>
