24cf9db5a9abaa1e93a1548d1daf7c5b268ff595
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-03 08:54:09 +0300

    liste des postes

--- frontend/src/components/Administrateur/CrudCollab/Postes.jsx
+++ frontend/src/components/Administrateur/CrudCollab/Postes.jsx
@@ -3,31 +3,44 @@ import { useState,useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+
 const Postes = () => {
+    
     const[listePoste,setListePoste]= useState([])
-    useEffect(()=>{
+     useEffect(()=>{
         axios.get('http://localhost:4000/api/poste/all_postes')
         .then((res) => {setListePoste(res.data)
                      console.log(listePoste)})
         .catch(err => console.log(err));
        }
        )
-  return (
-    <div className='poste'>
-     <h1>Les postes</h1>
-     <div className='block2'>
-  {listePoste.map(poste => (
-    <div className='com' key={poste.id}>
-      <div className='background-image'></div>
-      <h4>{poste.titrePoste}</h4>
-      <Link to={`/admin/postes/collab/${poste.id}`}>
-        <h5>Voir qui ont ce poste</h5>
-      </Link>
-    </div>
-  ))}
-</div>
-    </div>
-  )
+      return (
+        <div className='poste'>
+         <h1>Les postes</h1>
+         <div className="main">
+          <div className="sidebar">
+          <button className="ajout"><Link to='/admin/add'>Nouveau</Link></button>
+          <button className="ajout2"><Link to='/admin/listeCollab'>Collaborateurs</Link></button>
+          <button className="ajout2"><Link to='/admin/postes'>Postes</Link></button>
+          <button className="ajout2"><Link to='/admin/departements'>Departements</Link></button>
+          </div>
+
+            <div className="tableau">
+              <div className='block2'>
+                {listePoste.map(poste => (
+                  <div className='com' key={poste.id}>
+                    <div className='background-image'></div>
+                    <h4>{poste.titrePoste}</h4>
+                    <Link to={`/admin/postes/collab/${poste.id}`}>
+                      <h5>Voir qui ont ce poste</h5>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+        </div>
+        </div>
+    )
 }
 
 export default Postes
