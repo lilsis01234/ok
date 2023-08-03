18e42b9c67363515906a12702c93847b32403a6a
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-03 17:17:23 +0300

    formulaires

---
+++ frontend/src/components/Administrateur/CrudCollab/formulaire/AddCollab.jsx
@@ -0,0 +1,149 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+import {useNavigate } from 'react-router-dom';
+
+const AddCollab = () => {
+
+  const Sexe= [
+    {id:1,nom:'masculin'},
+    {id:2,nom:'feminin'}
+  ]
+  const navigate = useNavigate();
+  const [nom,setNom]=useState('');
+  const [photo,setPhoto]=useState(null);
+  const [prenom,setPrenom]=useState('');
+  const [lot,setLot]=useState('');
+  const [quartier,setQuartier]=useState('');
+  const [ville,setVille]=useState('');
+  const [telephone,setTelephone]=useState('');
+  const[matricule,setMatricule] = useState('');
+  const[dateNaissance,setdateNaissance]=useState('');
+  const[dateEmbauche,setdateEmbauche]=useState('');
+  const[site,setSite]=useState('');
+  const[poste,setPoste]=useState('');
+  const[sexe,setSexe]=useState('');
+  
+  const[listePoste,setListePoste]=useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:4000/api/poste/all_postes')
+    .then((res) => {setListePoste(res.data)
+                console.log(listePoste)})
+    .catch(err => console.log(err));
+  }
+  )
+ 
+  const Validation=(event)=>{
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('image',photo);
+    formData.append('nom',nom);
+    formData.append('lot',lot);
+    formData.append('quartier',quartier);
+    formData.append('ville',ville);
+    formData.append('dateEmbauche',dateEmbauche);
+    formData.append('site',site);
+    formData.append('dateNaissance',dateNaissance);
+    formData.append('poste',poste);
+    formData.append('sexe',sexe);
+    formData.append('prenom',prenom);
+    formData.append('telephone',telephone);
+    formData.append('matricule',matricule);
+    console.log(formData);
+
+    axios.post('http://localhost:4000/api/collaborateur/add',formData,{headers: {
+      'Content-Type': 'multipart/form-data',
+    },})
+    .then(res => {
+        console.log(res);
+        navigate('/admin/listeCollab');
+    }).catch(err =>console.log(err));
+  };
+
+  return (
+    <div className='block'>
+        <h1>Ajouter un collaborateur</h1>
+        <form onSubmit={Validation} className='add-form'>
+          <div className='add'>
+          <label className="add-collab">Photo:</label><br></br>
+          <input type='file' onChange={(e)=>setPhoto(e.target.files[0])} className='add-input'></input>
+          
+          <label className="add-collab">Nom:</label> <br></br>
+          <input type='text' onChange={(e)=>{setNom(e.target.value)}} className='add-input'></input>
+          </div>
+
+          <div className='add'>
+          <label className="add-collab">Prenom:</label><br></br>  
+          <input type='text'onChange={(e)=>{setPrenom(e.target.value)}} className='add-input'></input>
+          
+          <label className="add-collab">Adresse:</label><br></br> 
+          <input type='text' onChange={(e)=>{setLot(e.target.value)}} className='add-input'></input>
+          </div>
+
+          <div className='add'>
+          <label className="add-collab">Quartier:</label><br></br>    
+          <input type='text' onChange={(e)=>{setQuartier(e.target.value)}} className='add-input'></input>
+          
+          <label className="add-collab">Ville:</label><br></br>    
+          <input type='text' onChange={(e)=>{setVille(e.target.value)}} className='add-input'></input>
+          </div>
+
+          <div className='add'>
+          <label className="add-collab"> Téléphone:</label><br></br>  
+          <input type='text' onChange={(e)=>{setTelephone(e.target.value)}} className='add-input'></input>
+          
+          <label className="add-collab"> Matricule:</label><br></br>  
+          <input type='text' onChange={(e)=>{setMatricule(e.target.value)}} className='add-input'></input>
+          </div>
+          
+          <div className='add'>
+          <label className="add-collab"> Date de naissance:</label><br></br>  
+          <input type='date' onChange={(e)=>{setdateNaissance(e.target.value)}} className='add-input'></input>
+          
+          <label className='add-collab'>Poste</label>
+          <select
+            value={poste}
+            onChange={(e) => setPoste(e.target.value)}
+            className='add-input'
+          >
+            {listePoste.map((poste) => (
+              <option key={poste.id} value={poste.id}>
+                {poste.titrePoste}
+              </option>
+            ))}
+          </select>
+          </div>
+
+          <div className='add'>
+          <label className="add-collab"> Date d'embauche:</label><br></br>  
+          <input type='date' onChange={(e)=>{setdateEmbauche(e.target.value)}} className='add-input'></input>
+          
+          <label className="add-collab"> Site:</label><br></br>  
+          <input type='text' onChange={(e)=>{setSite(e.target.value)}} className='add-input'></input>
+          </div>
+
+          <div className='add2'>
+          <label  className="add-collab">Sexe</label>
+          <select
+            value={sexe}
+            onChange={(e) => setSexe(e.target.value)}
+            className='add-input'
+          >
+            {Sexe.map((sexe) => (
+              <option key={sexe.id} value={sexe.nom}>
+                {sexe.nom}
+              </option>
+            ))}
+          </select>
+         </div> 
+
+          <button type='submit'>Envoyer</button>
+          </form>
+    </div>
+  )
+}
+
+
+export default AddCollab
