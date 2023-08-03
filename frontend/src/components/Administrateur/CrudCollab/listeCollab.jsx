093afcfff4ad5965a2a7f28505d0f7ccb085ca43
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-03 13:04:33 +0300

    liste des collaborateurs

--- frontend/src/components/Administrateur/CrudCollab/listeCollab.jsx
+++ frontend/src/components/Administrateur/CrudCollab/listeCollab.jsx
@@ -3,49 +3,92 @@ import { useState,useEffect } from "react";
 import axios from "axios";
 import './listCollab.css'
 import { Link } from "react-router-dom";
-import logo from '../../../image/logo_sahaza.png';
+import logo from '../../../image/sahaza.svg';
+import {FaPlus,FaListUl} from 'react-icons/fa';
+import {MdWorkspacesFilled,MdOutlineBusiness} from 'react-icons/md'
+
+
 
 const ListeCollab = () => {
 
   const[recherche,setRecherche]=useState('null');
+  const[recherchenom,setRechercheNom]=useState('null');
+  const[rechercheprenom,setRecherchePrenom]=useState('null');
+  const[recherchedatenaissance,setRechercheDatedeNaissance]=useState('null');
+  const[recherchematricule,setRechercheMatricule]=useState('null');
+  const[rechercheadresse,setRechercheAdresse]=useState('null');
+  const[rechercheposte,setRecherchePoste]=useState('null');
+  const[recherchedepartement,setRechercheDepartement]=useState('null');
+  const[recherchetelephone,setRechercheTelephone]=useState('null');
+  
+
   const [Collabs, setCollabs] = useState([]);
-  // const[Poste,setPoste]=useState([])
- if(recherche===''){
-  setRecherche('null')
- }
-  useEffect(()=> {
-    axios.get('http://localhost:4000/api/collaborateur/all_collaborateurs')
-    .then(res => setCollabs(res.data))
-    .catch(err => console.log(err));
-  },[])  
+  if(recherche===''){
+    setRecherche('null')
+  }
+  if(recherchenom===''){
+    setRechercheNom('null')
+  }
+  if(rechercheprenom===''){
+    setRecherchePrenom('null')
+  }
+  if(recherchedatenaissance===''){
+    setRechercheDatedeNaissance('null')
+  }
+  if(recherchematricule===''){
+    setRechercheMatricule('null')
+  }
+  if(rechercheadresse===''){
+    setRechercheAdresse('null')
+  }
+  if(rechercheposte===''){
+    setRecherchePoste('null')
+  }
+  if(recherchedepartement===''){
+    setRechercheDepartement('null')
+  }
+  if(recherchetelephone===''){
+    setRechercheTelephone('null')
+  }
+
+    useEffect(()=> {
+      axios.get('http://localhost:4000/api/collaborateur/all_collaborateurs')
+      .then(res => setCollabs(res.data))
+      .catch(err => console.log(err));
+    },[])  
 
   return (
     <div className="bodyliste">
+     
+      
+      <div className="main">
+      <div className="sidebar">
       <img className="logo" src={logo} alt='logo sahaza'/>
+      <input type='text' id='recherche'className="add-input" placeholder="rechercher" onChange={(e)=>setRecherche(e.target.value)}/>
+      <button className="ajout"><Link to='/admin/add'><FaPlus/></Link></button>
+      <button className="ajout2"><Link to='/admin/listeCollab'><FaListUl/></Link></button>
+      <button className="ajout2"><Link to='/admin/postes'><MdWorkspacesFilled/></Link></button>
+      <button className="ajout2"><Link to='/admin/departements'><MdOutlineBusiness/></Link></button>
+     
+      </div>
+
+     <div className="tableau">
       <h1>Liste des Collaborateurs</h1>
-      
-      
-      <input type='text' id='recherche'className="add-input" placeholder="rechercher ici" onChange={(e)=>setRecherche(e.target.value)}/>
-    
-    
-    <button className="ajout"><Link to='/admin/add'>+</Link></button>
-    <button className="ajout2"><Link to='/admin/postes'>Les postes</Link></button>
-    <table className="listeCollabo">
-      {Collabs.filter(collab=>collab.nom.includes(recherche)).map(Collab=>(
-       <tr key={Collab.id}>
-         <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
-         <td>{Collab.matricule}</td>
-         <td>{Collab.nom}</td>
-         <td>{Collab.prenom}</td>
-         <td>{Collab.lot}</td>
-         <td>{Collab.tel}</td>
-         <td>{Collab.dateNaissance}</td>
-         <td>{Collab.titrePoste}</td>
-         <td>{Collab.departement}</td>
-         <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
-         </tr>
-      ))}
-    </table>
+    <div  className="listeCollabo">
+        {Collabs.filter(collab=>collab.nom.includes(recherche)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
 
 
       <table className="listeCollabo">
@@ -64,7 +107,154 @@ const ListeCollab = () => {
         </tr>
         </thead>
         <tbody>
-          {Collabs.map(Collab =>
+        <tr>
+            <td></td>
+            <td><input type='text' id='filtre'className="add-input" placeholder="matricule" onChange={(e)=>setRechercheMatricule(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="nom" onChange={(e)=>setRechercheNom(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="prenom" onChange={(e)=>setRecherchePrenom(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="adresse" onChange={(e)=>setRechercheAdresse(e.target.value)}/></td>
+            <td><input type='text' id='filtre'className="add-input" placeholder="telephone" onChange={(e)=>setRechercheTelephone(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="date de naissance" onChange={(e)=>setRechercheDatedeNaissance(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="poste" onChange={(e)=>setRecherchePoste(e.target.value)}/></td>
+            <td><input type='text'id='filtre'className="add-input" placeholder="departement" onChange={(e)=>setRechercheDepartement(e.target.value)}/></td>
+        </tr>
+
+        {Collabs.filter(collab=>collab.matricule.includes(recherchematricule)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.nom.includes(recherchenom)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.prenom.includes(rechercheprenom)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.lot.includes(rechercheadresse)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.lot.includes(rechercheadresse)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.tel.includes(recherchetelephone)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.dateNaissance.includes(recherchedatenaissance)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.titrePoste.includes(rechercheposte)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.filter(collab=>collab.departement.includes(recherchedepartement)).map(Collab=>(
+        <tr key={Collab.id}>
+          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+          <td>{Collab.matricule}</td>
+          <td>{Collab.nom}</td>
+          <td>{Collab.prenom}</td>
+          <td>{Collab.lot}</td>
+          <td>{Collab.tel}</td>
+          <td>{Collab.dateNaissance}</td>
+          <td>{Collab.titrePoste}</td>
+          <td>{Collab.departement}</td>
+          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+          </tr>
+        ))}
+
+        {Collabs.map(Collab =>
            <tr key={Collab.id}>
             <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
             <td>{Collab.matricule}</td>
@@ -77,9 +267,12 @@ const ListeCollab = () => {
             <td>{Collab.departement}</td>
             <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
             </tr>
-          )}
-          </tbody>    
+        )}
+        </tbody>    
         </table>
+        </div>
+        </div>
+    </div>
     </div>
   )
 }
