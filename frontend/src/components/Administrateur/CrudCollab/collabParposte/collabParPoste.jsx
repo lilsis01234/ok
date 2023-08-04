8de4b20ebe983c4d673b4156e37f4955ce580c73
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-04 15:40:46 +0300

    collaborateur par poste

--- frontend/src/components/Administrateur/CrudCollab/collabParposte/collabParPoste.jsx
+++ frontend/src/components/Administrateur/CrudCollab/collabParposte/collabParPoste.jsx
@@ -11,12 +11,9 @@ const CollabParPoste = () => {
 
   
    const { idposte } = useParams();
-   const[recherche,setRecherche]=useState('null');
+   const[recherche,setRecherche]=useState('');
    const [Collabs, setCollabs] = useState([]);
   
-    if(recherche===''){
-      setRecherche('null')
-    }
     useEffect(() => {
     console.log('Fetching data for poste:', idposte); // Make sure poste is defined here
     axios.get(`http://localhost:4000/api/collaborateur/all_collaborateurs/${idposte}`)
@@ -45,27 +42,8 @@ const CollabParPoste = () => {
       </div>
 
         <div className='tableau'>
-        <div  className="listeCollabo">
-
-
-        {Collabs.filter(collab=>collab.nom.includes(recherche)).map(Collab=>(
-        <tr key={Collab.id}>
-          <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
-          <td>{Collab.matricule}</td>
-          <td>{Collab.nom}</td>
-          <td>{Collab.prenom}</td>
-          <td>{Collab.lot}</td>
-          <td>{Collab.tel}</td>
-          <td>{Collab.dateNaissance}</td>
-          <td>{Collab.titrePoste}</td>
-          <td>{Collab.departement}</td>
-          <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
-        </tr>
-        ))}
-
-
-
-          <table className="listeCollabo">
+        <div className='listeCollabo'>
+          <table>
           <thead>
           <tr>
               <th>Photo</th>
@@ -81,7 +59,7 @@ const CollabParPoste = () => {
           </tr>
           </thead>
           <tbody>
-            {Collabs.map(Collab =>
+           {recherche === '' ? (Collabs.map(Collab =>
             <tr key={Collab.id}>
               <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
               <td>{Collab.matricule}</td>
@@ -94,12 +72,27 @@ const CollabParPoste = () => {
               <td>{Collab.departement}</td>
               <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
               </tr>
-            )}
+            )):
+             (Collabs.filter(collab=>collab.nom.includes(recherche)).map(Collab=>(
+                <tr key={Collab.id}>
+                <td><img src={`http://localhost:4000/${Collab.image}`} alt={Collab.nom}/></td>
+                <td>{Collab.matricule}</td>
+                <td>{Collab.nom}</td>
+                <td>{Collab.prenom}</td>
+                <td>{Collab.lot}</td>
+                <td>{Collab.tel}</td>
+                <td>{Collab.dateNaissance}</td>
+                <td>{Collab.titrePoste}</td>
+                <td>{Collab.departement}</td>
+                <td><button className="update"><Link to={`/admin/update/${Collab.id}`}>Modifier</Link></button></td>
+              </tr>
+              )))}
+
           </tbody>    
           </table>
           </div>
           </div>
-      </div>
+          </div>
       </div>
     )
 }
