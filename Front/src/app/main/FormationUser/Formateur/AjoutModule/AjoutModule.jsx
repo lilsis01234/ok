8981f97cdf9702def96e08588aa0373c60eab0f1
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-10-05 17:30:56 +0300

    fichiers supprimés

--- Front/src/app/main/FormationUser/Formateur/AjoutModule/AjoutModule.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AjoutModule = () => {
-  return (
-    <div>AjoutModule</div>
-  )
-}
-
-export default AjoutModule
--- Front/src/app/main/FormationUser/Formateur/AjoutSeance/AjoutSeance.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AjoutSeance = () => {
-  return (
-    <div>AjoutSeance</div>
-  )
-}
-
-export default AjoutSeance
