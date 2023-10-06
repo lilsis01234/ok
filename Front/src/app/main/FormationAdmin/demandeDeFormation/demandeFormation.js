b905c3245ceb23cef5039639a53e53fcb6f6a954
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-10-06 15:55:29 +0300

    initialisation

--- Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
@@ -1,12 +1,15 @@
 import React,{useState,useEffect} from 'react'
-
+import axios from 'axios'
 
 const DemandeFormations = () => {
   const[DemandeFormations,setDemandes] = useState([])
+
+  useEffect(()=>{
+
+  })
   return (
     <div>
         <h1 className="collabListes_title font-bold">Demandes de formations</h1>
-
     </div>
   )
 }
