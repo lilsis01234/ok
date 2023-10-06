fd2a56f1994a4fe102ed1883e3bbefe3c1a91bd3
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-10-06 16:43:24 +0300

    liste des demandes de formations

--- Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
@@ -5,11 +5,16 @@ const DemandeFormations = () => {
   const[DemandeFormations,setDemandes] = useState([])
 
   useEffect(()=>{
-
+    axios.get('http://localhost:4001/api/demande_formation/approuveparRrh')
+    .then(res=>setDemandes(res.data))
+    .catch(err=>console.log(err))
   })
   return (
     <div>
         <h1 className="collabListes_title font-bold">Demandes de formations</h1>
+        {DemandeFormations.map((demande)=>(
+          <h1>{demande.nom}</h1>
+        ))}
     </div>
   )
 }
