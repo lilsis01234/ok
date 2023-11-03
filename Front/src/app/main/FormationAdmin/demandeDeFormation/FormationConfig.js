c7ea4f534a86a6bd8f25ac8d6eb97e984d0e03b5
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-03 14:55:32 +0300

    [ADD] Lien vers discussion

--- Front/src/app/main/FormationAdmin/demandeDeFormation/FormationConfig.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/FormationConfig.js
@@ -6,7 +6,7 @@ const VoirPlusFormation = lazy(()=>import('../VoirPlus/VoirPlusFormation'))
 const AjoutFormation = lazy(()=>import('../../FormationUser/Formateur/AjoutFormation/AjoutFormations'))
 const AjoutModule = lazy(()=>import('../../FormationUser/Formateur/AjoutModule/AjoutModule'))
 const CalendarForm = lazy(() => import('../../CalendarSeance/calendrierForm/CalendarForm'))
-
+const Discussions = lazy(()=>import('../discussionFormation/discussionFormation'))
 
 const FormationConfig = {
     routes: [
@@ -38,6 +38,10 @@ const FormationConfig = {
                     path:'formation/addModule',
                     element: <AjoutModule/>
                 },
+                {
+                    path:'/discussion/formation/:id',
+                    element:<Discussions/>
+                }
             ],
 };
 
