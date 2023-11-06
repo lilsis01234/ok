82cb84b1cc10a1c78640bc6fb352ae49be68189a
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-06 11:55:40 +0300

    synchronisation bdd

--- backend/database/sync-database.js
+++ backend/database/sync-database.js
@@ -19,17 +19,42 @@ const CommentaireFormation = require('../Modele/formation/CommentaireFormation')
 const DiscussionFormation= require('../Modele/formation/DiscussionFormation');
 const Module = require('../Modele/formation/Module');
 const Seance = require('../Modele/formation/Seance');
+
 const ParticipantsSeance = require('../Modele/formation/ParticipantsSeance');
+const EquipeSeance = require('../Modele/formation/EquipeSeance');
+const FormationCollab = require('../Modele/formation/FormationCollab');
+const FormationDep = require('../Modele/formation/FormationDep');
 
-
+const associationSeanceCollab = require('../Modele/formation/associationSeanceCollab');
+const associationFormationEq = require('../Modele/formation/associationFormationDep');
+const associationFormationCollab= require('../Modele/formation/associationFormationCollab');
+const associationSeanceEquipe = require('../Modele/formation/associationSeanceEquipe');
 
 //Synchronisation de la base de donnée 
 async function syncDatabase(){
     try{
         await sequelize.sync({force : true}); 
+
         const { TestPoste, TestDepartement, PosteDepartement } = association;
         TestPoste.belongsToMany(TestDepartement, { through: PosteDepartement });
         TestDepartement.belongsToMany(TestPoste, { through: PosteDepartement });
+
+        const{ Seance, Collaborateur ,ParticipantsSeance} = associationSeanceCollab;
+        Collaborateur.belongsToMany(Seance,{through:ParticipantsSeance});
+        Seance.belongsToMany(Collaborateur,{through:ParticipantsSeance});
+
+        const{ Seance2, Equipe,EquipeSeance} = associationSeanceEquipe;
+        Equipe.belongsToMany(Seance2,{through:EquipeSeance});
+        Seance2.belongsToMany(Equipe,{through:EquipeSeance});
+
+        const{Formation2,Collab2,FormationCollab} = associationFormationCollab;
+        Formation2.belongsToMany(Collab2,{through:FormationCollab});
+        Collab2.belongsToMany(Formation2,{through:FormationCollab});
+
+        const{Formation,Equipe2,FormationEq} = associationFormationEq;
+        Formation.belongsToMany(Equipe2,{through:FormationEq});
+        Equipe2.belongsToMany(Formation,{through:FormationEq});
+
         console.log('La base de donnée est synchronisée avec succès')
     }  catch (error){
         console.error('Erreur lors de la synchronisation de la base de données :', error )
