13bb19423ba02a7c325c2542052af8cf5e23d015
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-14 17:31:04 +0300

    Dashboard

--- Front/src/app/Home/Dashboard.js
+++ Front/src/app/Home/Dashboard.js
@@ -8,9 +8,6 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 
-
-
-
 const Dashboard = () => {
 
 
@@ -21,14 +18,33 @@ const Dashboard = () => {
           <div className="flex justify-center w-full p-24 sm:p-32">
             <div className="w-full px-24 md:px-32 pb-24">
               <div className="md:flex">
+
                 {/* Column 1 */}
                 <div className="flex flex-col w-full md:w-1/2">
                   <div className="flex-auto">
                     <img src={imageTest} className="rounded-xl" alt="Sahaza Group" />
                   </div>
-                  <div className="flex-auto w-200">Evènement approchant</div>
-                  <div className="flex-auto w-200">Anniversaires du mois</div>
-                  <div className="flex-auto w-200">Vidéo</div>
+                  <div className="flex-auto w-full rounded-sm h-48 bg-grey-100 text-center mt-10">
+                    <Paper className='h-48 w-full'>
+                    <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-red-700 mt-10">
+                        Soon will be Christmas Day 🎉🎆
+                      </Typography>
+                    </Paper>
+                  </div>
+
+                  <div className="flex-auto w-full h-98 rounded-sm bg-grey-100 text-center mt-10">
+                  <Paper className='w-full h-98 aspect-w-1'>
+                    <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-red-700 mt-20 mb-30">
+                        Anniversaires du mois🎉🎆
+                    </Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque lacus id enim lobortis imperdiet. Sed mauris massa, tincidunt efficitur lorem vitae, tristique lacinia velit. Donec dignissim suscipit ex, vehicula finibus magna congue nec. Donec tempus vitae felis vel dapibus. Ut hendrerit vitae magna nec malesuada.
+                  </Paper>
+                  </div>
+
+                  <div className="flex-auto mt-50">
+                    <img src={imageTest} className="rounded-xl mt-50" alt="Sahaza Group" />
+                  </div>
+
                   <div className="flex-auto w-200">Les 3 directions</div>
                   <div className="flex-auto w-200">Liste des formations</div>
                 </div>
@@ -49,6 +65,32 @@ const Dashboard = () => {
                     </Typography>
                   </Paper>
 
+                  <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-6 overflow-hidden aspect-w-1 ml-10 mt-10">
+                    <div className="text-center mt-8">
+                      <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-green-400">
+                        Actualités
+                      </Typography>
+                    </div>
+                    <Typography
+                      className="flex items-baseline justify-center w-full mt-20"
+                      color="text.secondary"
+                    >
+                      <b className="px-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque lacus id enim lobortis imperdiet. Sed mauris massa, tincidunt efficitur lorem vitae, tristique lacinia velit. Donec dignissim suscipit ex, vehicula finibus magna congue nec. Donec tempus vitae felis vel dapibus. Ut hendrerit vitae magna nec malesuada.</b>
+                    </Typography>
+                  </Paper>
+                  <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-6 overflow-hidden aspect-w-1 ml-10 mt-10">
+                    <div className="text-center mt-8">
+                      <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-green-400">
+                        Actualités
+                      </Typography>
+                    </div>
+                    <Typography
+                      className="flex items-baseline justify-center w-full mt-20"
+                      color="text.secondary"
+                    >
+                      <b className="px-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque lacus id enim lobortis imperdiet. Sed mauris massa, tincidunt efficitur lorem vitae, tristique lacinia velit. Donec dignissim suscipit ex, vehicula finibus magna congue nec. Donec tempus vitae felis vel dapibus. Ut hendrerit vitae magna nec malesuada.</b>
+                    </Typography>
+                  </Paper>
                   <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-6 overflow-hidden aspect-w-1 ml-10 mt-10">
                     <div className="text-center mt-8">
                       <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-green-400">
