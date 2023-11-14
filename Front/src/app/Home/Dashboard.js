25eeb5b117b2cf3b72f731bdc24646719660234d
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-14 16:55:15 +0300

    dashboard

--- Front/src/app/Home/Dashboard.js
+++ Front/src/app/Home/Dashboard.js
@@ -1,22 +1,86 @@
 import FusePageSimple from '@fuse/core/FusePageSimple';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import imageTest from '../../../../Front/src/image_test.jpg';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-function Dashboard(){
-    return (
-        <FusePageSimple
-        content={
-        <div className="w-full px-24 md:px-32 pb-24">
-            <div className='block'>
-                <div className='flex'>
-                    <div className='flex-auto w-200 h-96'>Photo collab </div>
-                    <div className='flex-auto w-100 h-80'>Mot du jour</div>
-                    <div className='flex-auto w-100 h-96'>Calendrier</div>
+
+
+
+
+const Dashboard = () => {
+
+
+  return (
+    <FusePageSimple
+      content={
+        <>
+          <div className="flex justify-center w-full p-24 sm:p-32">
+            <div className="w-full px-24 md:px-32 pb-24">
+              <div className="md:flex">
+                {/* Column 1 */}
+                <div className="flex flex-col w-full md:w-1/2">
+                  <div className="flex-auto">
+                    <img src={imageTest} className="rounded-xl" alt="Sahaza Group" />
+                  </div>
+                  <div className="flex-auto w-200">Evènement approchant</div>
+                  <div className="flex-auto w-200">Anniversaires du mois</div>
+                  <div className="flex-auto w-200">Vidéo</div>
+                  <div className="flex-auto w-200">Les 3 directions</div>
+                  <div className="flex-auto w-200">Liste des formations</div>
                 </div>
-                <div className='flex'>
-                    <div className='flex-auto w-200 h-24'> Evènement approchant</div>
-                    <div className='flex-auto w-100 h-64'> Actualités</div>
+
+                {/* Column 2 */}
+                <div className="flex flex-col w-full md:w-1/4 ml-10">
+                  <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-6 ml-10 overflow-hidden aspect-w-1">
+                    <div className="text-center mt-8">
+                      <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-red-400 mt-60">
+                        Mot du jour
+                      </Typography>
+                    </div>
+                    <Typography
+                      className="flex items-baseline justify-center w-full mt-20 mb-24"
+                      color="text.secondary"
+                    >
+                      <b className="px-8">Exemple de mot du jour</b>
+                    </Typography>
+                  </Paper>
+
+                  <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-6 overflow-hidden aspect-w-1 ml-10 mt-10">
+                    <div className="text-center mt-8">
+                      <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-green-400">
+                        Actualités
+                      </Typography>
+                    </div>
+                    <Typography
+                      className="flex items-baseline justify-center w-full mt-20"
+                      color="text.secondary"
+                    >
+                      <b className="px-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque lacus id enim lobortis imperdiet. Sed mauris massa, tincidunt efficitur lorem vitae, tristique lacinia velit. Donec dignissim suscipit ex, vehicula finibus magna congue nec. Donec tempus vitae felis vel dapibus. Ut hendrerit vitae magna nec malesuada.</b>
+                    </Typography>
+                  </Paper>
                 </div>
+
+                {/* Column 3 */}
+                <div className="flex flex-col w-full md:w-1/4 ml-8">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar />
+                </LocalizationProvider>
+
+                <div className="flex-auto">Graphe 3 sites</div>
+                <div className="flex-auto">Citation sur SAHAZA GROUP</div>
+                <div className="flex-auto">Resultats des compétitions</div>
+                </div>
+              </div>
             </div>
-        </div>}/>
-    )
-}
-export default Dashboard
+          </div>
+        </>
+      }
+    />
+  );
+};
+
+export default Dashboard;
