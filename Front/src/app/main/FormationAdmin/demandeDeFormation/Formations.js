9c2f562d51a98dbf1c31d1f00a5b11af5861ee91
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-15 17:29:03 +0300

    idk what

--- Front/src/app/main/FormationAdmin/demandeDeFormation/Formations.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/Formations.js
@@ -23,74 +23,107 @@ const Formations = () => {
 
 
   const [recherche,setRecherche] = useState(null);
+  
   return (
-    <>
-    <div className='header'>
-      <h1>Explorez nos Formations</h1>
-      <div className="search_form">
-        <input
-          type='text'
-          placeholder='Rechercher ici...'
-          onChange={(e) => {
-            setRecherche(e.target.value);
-          }}
-        />
-      </div>
-    </div>
-    <div className="training_container">
-      {Formation.length !== 0 ? (
 
-        recherche === ''||recherche === null ?(
+          <>
+          <div className='header'>
+            <h1>Explorez nos Formations</h1>
 
-        Formation.map((formation) => (
-          <div key={formation.id} className="formation_item">
-           <Link to={`/admin/formation/${formation.id}`}><h2 className="formation_title">{formation.theme}</h2></Link>
-            <Typography className="formation_description">{formation.description}</Typography>
-            {formation.Formateur ? (
-              <Typography className="formateur_name">
-                Formateur: {formation.Formateur.nom} {formation.Formateur.prenom}
-              </Typography>
-            ) : (
-              <Typography className="formateur_name">Formateur externe</Typography>
-            )}
-            <Link to={`/discussion/formation/${formation.id}`}><span className="lien">Accéder à la discussion</span></Link>
-
-            <button className="voir_plus_button">
-              <Link to={`/admin/formation/${formation.id}`}>?</Link>
-            </button>
-          </div>
-        ))
-        
-        ):(
-
-          Formation.filter((formation) => (
-            formation.theme.toLowerCase().includes(recherche.toLowerCase()) ||
-            formation.description.toLowerCase().includes(recherche.toLowerCase()) ||
-            (formation.Formateur && formation.Formateur.nom && formation.Formateur.nom.toLowerCase().includes(recherche.toLowerCase()))||
-            (formation.Formateur && formation.Formateur.prenom && formation.Formateur.prenom.toLowerCase().includes(recherche.toLowerCase()))
-          )).map((formation) => (
-            <div key={formation.id} className="formation_item">
-               <h2 className="formation_title"><Link to={`/admin/formation/${formation.id}`}>{formation.theme}</Link></h2>
-              <Typography className="formation_description">{formation.description}</Typography>
-              {formation.Formateur ? (
-                <Typography className="formateur_name">
-                  Formateur: {formation.Formateur.nom} {formation.Formateur.prenom}
-                </Typography>
-              ) : (
-                <Typography className="formateur_name">Formateur externe</Typography>
-              )}
-              <button className="voir_plus_button">
-                <Link to={`/admin/formation/${formation.id}`}>?</Link>
-              </button>
+            <div className="search_form">
+              
+              <input
+                type='text'
+                placeholder='Rechercher ici...'
+                onChange={(e) => {
+                  setRecherche(e.target.value);
+                }}
+              />
             </div>
-        )))
 
-        ) : (
-        <h3>Aucune formation pour le moment</h3>
-        )}
-    </div>
-    </>
-  )
+          </div>
+          
+          <div className="training_container">
+            
+            {Formation.length !== 0 ? (
+
+              recherche === ''||recherche === null ?(
+
+              Formation.map((formation) => (
+                
+                <div key={formation.id} className="formation_item">
+                
+                <Link to={`/admin/formation/${formation.id}`}><h2 className="formation_title">{formation.theme}</h2></Link>
+                  
+                  <Typography className="formation_description">{formation.description}</Typography>
+                  
+                  {formation.Formateur ? (
+                    
+                    <Typography className="formateur_name">
+                      Formateur: {formation.Formateur.nom} {formation.Formateur.prenom}
+                    </Typography>
+                  
+                  ) 
+                  : 
+                  (
+                    <Typography className="formateur_name">Formateur externe</Typography>
+                  )
+                  }
+                  
+                  <Link to={`/discussion/formation/${formation.id}`}><span className="lien">Accéder à la discussion</span></Link>
+
+                  <button className="voir_plus_button">
+                    <Link to={`/admin/formation/${formation.id}`}>?</Link>
+                  </button>
+                
+                </div>
+              ))
+              
+              ):(
+
+                Formation.filter((formation) => (
+                  
+                  formation.theme.toLowerCase().includes(recherche.toLowerCase()) ||
+                  formation.description.toLowerCase().includes(recherche.toLowerCase()) ||
+                  (formation.Formateur && formation.Formateur.nom && formation.Formateur.nom.toLowerCase().includes(recherche.toLowerCase()))||
+                  (formation.Formateur && formation.Formateur.prenom && formation.Formateur.prenom.toLowerCase().includes(recherche.toLowerCase()))
+                
+                )).map((formation) => (
+
+                  <div key={formation.id} className="formation_item">
+                    
+                    <h2 className="formation_title"><Link to={`/admin/formation/${formation.id}`}>{formation.theme}</Link></h2>
+                    
+                    <Typography className="formation_description">{formation.description}</Typography>
+                    
+                    {formation.Formateur ? (
+                      
+                      <Typography className="formateur_name">
+                        Formateur: {formation.Formateur.nom} {formation.Formateur.prenom}
+                      </Typography>
+                    
+                    ) 
+                    : 
+                    (
+                      <Typography className="formateur_name">Formateur externe</Typography>
+                    )}
+                    
+                    <button className="voir_plus_button">
+                      <Link to={`/admin/formation/${formation.id}`}>?</Link>
+                    </button>
+                  
+                  </div>
+              )))
+
+              ) 
+              : 
+              (
+                <h3>Aucune formation pour le moment</h3>
+              )}
+          </div>
+          </>
+
+        )
 }
 
 export default Formations
