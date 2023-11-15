6ecd50cd1bf8aa95408ad06e13e297ca70b43492
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-15 16:13:05 +0300

    liste demandes de formations

--- Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
@@ -1,20 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
+import './demandeFormation.css'
+import { Link } from 'react-router-dom'
 
 const DemandeFormations = () => {
   const[DemandeFormations,setDemandes] = useState([])
 
   useEffect(()=>{
-    axios.get('http://localhost:4001/api/demande_formation/approuveparRrh')
-    .then(res=>setDemandes(res.data))
+    axios.get('http://localhost:4000/api/demande_formation/all')
+    .then((res)=>
+      { setDemandes(res.data)
+        console.log(res.data)
+      })
     .catch(err=>console.log(err))
   })
+  
   return (
-    <div>
-        <h1 className="collabListes_title font-bold">Demandes de formations</h1>
-        {DemandeFormations.map((demande)=>(
-          <h1>{demande.nom}</h1>
-        ))}
+    <div className="training-request-container">
+      <h1 className="collabListes_title font-bold">Demandes de formations</h1>
+      {DemandeFormations.map((demande, index) => (
+        <div key={index} className="training-request-item">
+          <h1 className="name">{demande.Auteur.nom} {demande.Auteur.prenom}</h1>
+          <h2 className="theme">{demande.theme}</h2>
+          {/* <p className="description">{demande.description}</p> */}
+          <Link to={`/voirPlus/demande/${demande.id}`} className="description">Voir plus </Link>
+        </div>
+      ))}
     </div>
   )
 }
