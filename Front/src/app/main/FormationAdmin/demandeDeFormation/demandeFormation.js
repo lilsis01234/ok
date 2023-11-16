541ef8d67c646cae4c479150a2fcfd6eb153ae27
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-16 09:47:18 +0300

    affichage mes demandes

--- Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
+++ Front/src/app/main/FormationAdmin/demandeDeFormation/demandeFormation.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import './demandeFormation.css'
 import { Link } from 'react-router-dom'
 import { Typography} from '@mui/material'
+import MesDemandes from './MesDemandes'
 
 
 const DemandeFormations = () => {
@@ -20,11 +21,16 @@ const DemandeFormations = () => {
   return (
     <div className="training-request-container">
       <h1 className="collabListes_title font-bold">Demandes de formations</h1>
+      <Typography>Vos demandes</Typography>
+
+      <MesDemandes/>
+
+      <Typography>Les demandes de formation</Typography>
+
       {DemandeFormations.map((demande, index) => (
         <div key={index} className="training-request-item">
           <Typography className="name">{demande.Auteur.nom} {demande.Auteur.prenom}</Typography>
           <Typography className="theme">{demande.theme}</Typography>
-          {/* <p className="description">{demande.description}</p> */}
           <Link to={`/voirPlus/demande/${demande.id}`} className="description">Voir plus </Link>
           {/* <button onClick={Approuver(demande.id)}>Approuver</button>
           <button onClick={Desapprouver(demande.id)}>Approuver</button> */}
