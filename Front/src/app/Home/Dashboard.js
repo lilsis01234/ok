983640528b613b58a7a5d56b2863de081cf4f3b2
toavina-fred <fredericoandria@gmail.com>
2023-11-17 15:44:09 +0300

    commit 3 17112023

--- Front/src/app/Home/Dashboard.js
+++ Front/src/app/Home/Dashboard.js
@@ -2,14 +2,12 @@ import FusePageSimple from '@fuse/core/FusePageSimple';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import React from 'react';
-import imageTest from '../../../../Front/src/image_test.jpg';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { motion } from 'framer-motion';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-
 
 const Dashboard = () => {
 
@@ -29,7 +27,6 @@ const Dashboard = () => {
                 {/* Column 1 */}
                 <div className="flex flex-col w-full md:w-1/2 px-16">
                   <div className="flex-auto mb-32">
-                    <img src={imageTest} className="rounded-xl" alt="Sahaza Group" />
                   </div>
                   <div className="flex-auto w-full rounded-sm h-48 bg-grey-100 text-center mb-32">
                     <Paper className='h-48 w-full'>
@@ -49,7 +46,6 @@ const Dashboard = () => {
                   </div>
 
                   <div className="flex-auto mb-32">
-                    <img src={imageTest} className="rounded-xl" alt="Sahaza Group" />
                   </div>
 
                   <div className="flex-auto w-200">Les 3 directions</div>
@@ -59,17 +55,19 @@ const Dashboard = () => {
                 {/* Column 2 */}
                 <div className="flex flex-col w-full md:w-1/4 px-16">
                   <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-32 overflow-hidden aspect-w-1">
-                    <div className="text-center mt-8">
-                      <Typography className="text-xl sm:text-3xl font-bold tracking-tight leading-none text-red-400 mt-60">
-                        Mot du jour
+                    <div className="h-224 bg-cover bg-no-repeat w-full" style={{ backgroundImage: `url(http://localhost:4000/uploads/nom-1700224214983-97175974.jpg)` }} >                      
+                      <div className="text-center mt-8" >
+                        <Typography className="text-xl sm:text-3xl font-bold tracking-tight text-white leading-none mt-60">
+                          Mot du jour
+                        </Typography>
+                      </div>
+                      <Typography
+                        className="flex items-baseline justify-center w-full mt-20 mb-24 text-white"
+                        color="text.secondary"
+                      >
+                        <b className="px-8">Exemple de mot du jour</b>
                       </Typography>
                     </div>
-                    <Typography
-                      className="flex items-baseline justify-center w-full mt-20 mb-24"
-                      color="text.secondary"
-                    >
-                      <b className="px-8">Exemple de mot du jour</b>
-                    </Typography>
                   </Paper>
 
                   <Paper className="flex flex-col flex-auto shadow rounded-2xl mb-32 overflow-hidden aspect-w-1">
@@ -116,8 +114,11 @@ const Dashboard = () => {
                 {/* Column 3 */}
                 <div className="flex flex-col w-full md:w-1/4 px-16">
                   <Card component={motion.div} variants={item} className="flex flex-col w-full mb-32">
-                    <CardContent className="p-0">  
-                      <LocalizationProvider dateAdapter={AdapterDayjs} >
+                    <div className="h-224 mb-16 bg-cover bg-no-repeat w-full" style={{ backgroundImage: `url(http://localhost:4000/uploads/nom-1700223383862-775994187.jpg)` }}>
+
+                    </div>
+                    <CardContent className="!p-0">  
+                      <LocalizationProvider dateAdapter={AdapterDayjs} locale="fr" >
                         <DateCalendar />
                       </LocalizationProvider>
                     </CardContent>
