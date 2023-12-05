d3479939cc8046e344c0074c2b54ff905d983927
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-12-05 09:15:11 +0300

    deletion

--- backend/routes/formation/AfficheAgenda.js
+++ backend/routes/formation/AfficheAgenda.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const  Agenda  = require('../../Modele/formation/Seance');
+const Formation = require('../../Modele/formation/Formation');
 
 // Route GET pour récupérer toutes les dates de l'agenda
 router.get('/agenda', async (req, res) => {
   try {
     // Récupérez toutes les entrées de l'agenda depuis la base de données
-    const agendaEntries = await Agenda.findAll();
+    const agendaEntries = await Agenda.findAll({
+      include: {
+        model: Formation,
+        attributes: ['theme', 'auteur', 'formateur']
+      }
+    });
     // Retournez les entrées de l'agenda en tant que réponse JSON
     res.status(200).json(agendaEntries);
   } catch (error) {
