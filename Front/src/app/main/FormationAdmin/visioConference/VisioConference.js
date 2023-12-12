00912c863061cb4726163023f5331d3b512caff4
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-12-12 11:03:52 +0300

    visio auto

--- Front/src/app/main/FormationAdmin/visioConference/VisioConference.js
+++ Front/src/app/main/FormationAdmin/visioConference/VisioConference.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import Peer from 'peerjs';
+import { useParams } from 'react-router-dom';
 
 function VisioConference() {
   const [peerId, setPeerId] = useState('');
@@ -7,36 +8,10 @@ function VisioConference() {
   const remoteVideoRef = useRef(null);
   const currentUserVideoRef = useRef(null);
   const peerInstance = useRef(null);
+  const idParam = useParams();
+  const id = idParam.id
+  console.log(id)
 
-  useEffect(() => {
-    const peer = new Peer();
-
-    peer.on('open', (id) => {
-      setPeerId(id);
-    });
-
-    peer.on('call', (call) => {
-      handleIncomingCall(call);
-    });
-
-    peerInstance.current = peer;
-  }, []);
-
-  const handleIncomingCall = (call) => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((mediaStream) => {
-        currentUserVideoRef.current.srcObject = mediaStream;
-        currentUserVideoRef.current.play();
-        call.answer(mediaStream);
-        call.on('stream', function (remoteStream) {
-          remoteVideoRef.current.srcObject = remoteStream;
-          remoteVideoRef.current.play();
-        });
-      })
-      .catch((error) => {
-        console.error('Error accessing audio devices:', error);
-      });
-  };
 
   const call = (remotePeerId) => {
     navigator.mediaDevices.getUserMedia({ audio: true })
@@ -56,11 +31,43 @@ function VisioConference() {
       });
   };
 
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on('open', (id) => {
+      setPeerId(id);
+    });
+
+    peer.on('call', (call) => {
+      handleIncomingCall(call);
+    });
+
+    peerInstance.current = peer;
+    call(id);
+  }, [id]);
+
+  const handleIncomingCall = (call) => {
+    navigator.mediaDevices.getUserMedia({video:true, audio: true })
+      .then((mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+        call.answer(mediaStream);
+        call.on('stream', function (remoteStream) {
+          remoteVideoRef.current.srcObject = remoteStream;
+          remoteVideoRef.current.play();
+        });
+      })
+      .catch((error) => {
+        console.error('Error accessing audio devices:', error);
+      });
+  };
+
   return (
     <div className="App">
-      <h1>Votre id est {peerId}</h1>
-      <input type="text" value={remotePeerIdValue} onChange={(e) => setRemotePeerIdValue(e.target.value)} />
-      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      {/* <h1>Votre id est {peerId}</h1> */}
+      {/* <input type="text" value={remotePeerIdValue} onChange={(e) => setRemotePeerIdValue(e.target.value)} />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button> */}
       <div>
         <video ref={currentUserVideoRef} />
       </div>
