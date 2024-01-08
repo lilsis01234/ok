1849329ba80d08d401098f8462797e7c1c671ec1
toavina-fred <fredericoandria@gmail.com>
2024-01-08 15:06:26 +0300

    commit avant checkout 1-gestion-actus

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 //Initalisation de la base de donnée
-const sequelize = new Sequelize('mysql://127.0.0.1:3306/intranettest', { 
+const sequelize = new Sequelize('mysql://127.0.0.1:3306/intranet', { 
   username : 'root', 
   password :'',
   charset: 'utf8mb4'
