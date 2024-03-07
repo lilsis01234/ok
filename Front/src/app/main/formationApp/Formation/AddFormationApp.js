0800c64b73e7ee8bcb9af1457d4bc0b071a7ff02
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2024-03-07 11:54:30 +0300

    Fusion avec le module formation

---
+++ Front/src/app/main/formationApp/Formation/AddFormationApp.js
@@ -0,0 +1,17 @@
+import { useThemeMediaQuery } from '@fuse/hooks'
+import React from 'react'
+
+const AddFormationApp = () => {
+    const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'))
+   
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddFormationAppg
