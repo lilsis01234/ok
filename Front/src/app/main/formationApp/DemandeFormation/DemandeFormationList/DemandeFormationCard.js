4df32be15a03897f6521ca075b81adb0bd9d89dc
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2024-05-14 10:29:01 +0300

    Affichage Demande de formation

---
+++ Front/src/app/main/formationApp/DemandeFormation/DemandeFormationList/DemandeFormationCard.js
@@ -0,0 +1,14 @@
+import { Card, CardContent } from '@mui/material'
+import React from 'react'
+
+function DemandeFormationCard({demande}) {
+  return (
+   <Card className="flex flex-col h-384 shadow">
+        <CardContent className="flex flex-col flex-auto p-24">
+            
+        </CardContent>
+   </Card>
+  )
+}
+
+export default DemandeFormationCard
--- Front/src/app/main/formationApp/DemandeFormation/DemandeFormationList/MesDemandeFormationList.js
+++ Front/src/app/main/formationApp/DemandeFormation/DemandeFormationList/MesDemandeFormationList.js
@@ -1,17 +1,18 @@
 import FusePageSimple from '@fuse/core/FusePageSimple';
 import { useThemeMediaQuery } from '@fuse/hooks'
-import { Box, FormControl, InputLabel, Typography } from '@mui/material';
+import { Box, FormControl, FormControlLabel, InputLabel, Switch, TextField, Typography } from '@mui/material';
 import { selectUser } from 'app/store/userSlice';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
+import { DatePicker } from '@mui/x-date-pickers';
 
 function MesDemandeFormationList() {
     const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
 
     const [mesDemandes, setMesDemandes] = useState()
-    const [filteredData, setFilteredData] = useState(null);
+    const [selectedData, handleDateChange] = React.useState(new Date());
     const [searchText, setSearchText] = useState();
     const [hideApprouve, setHideApprouve] = useState(false);
 
@@ -35,6 +36,12 @@ function MesDemandeFormationList() {
     }, [])
 
 
+
+    function handleSearchText(event) {
+        setSearchText(event.target.value);
+    }
+
+
     return (
         <FusePageSimple
             header={
@@ -49,19 +56,97 @@ function MesDemandeFormationList() {
                         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0 } }}>
                             <Typography color="inherit" className="text-18 font-semibold">MES DEMANDES DE FORMATIONS</Typography>
                         </motion.div>
-        
+
                     </div>
                 </Box>
             }
             content={
                 <div className="flex flex-col flex-1 w-full mx-auto px-24 pt-24 sm:p-40">
-                    <div className="flex flex-col shrink-0 sm:flex-row items-center justify-between space-y-16 sm:space-y-0"> 
+                    <div className="flex flex-col shrink-0 sm:flex-row items-center justify-between space-y-16 sm:space-y-0">
                         <div className="flex flex-col sm:flex-row w-full sm:w-auto items-center space-y-16 sm:space-y-0 sm:space-x-16">
                             <FormControl className="flex w-full sm:w-136" variant="outlined">
-                               <InputLabel  id="category-select-label">Date </InputLabel>     
+                                <DatePicker
+                                    label="Date"
+                                    value={selectedData}
+                                    onChange={handleDateChange}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
                             </FormControl>
+                            <TextField
+                                label="Rechercher une demande de formation"
+                                placeholder="Entrer un mot clé..."
+                                className="flex w-full sm:w-256 mx-8"
+                                value={searchText}
+                                inputProps={{
+                                    'aria-label': 'Search',
+                                }}
+                                onChange={handleSearchText}
+                                variant="outlined"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
                         </div>
+                        <FormControlLabel
+                            label="Masquer les demandes approuvées"
+                            control={
+                                <Switch
+                                    onChange={(ev) => {
+                                        setHideApprouve(ev.target.checked);
+                                    }}
+                                    checked={hideApprouve}
+                                    name="hideApprouve"
+                                />
+                            }
+                        />
                     </div>
+                    {useMemo(() => {
+                        const container = {
+                            show: {
+                                transition: {
+                                    staggerChildren: 0.1,
+                                }
+                            }
+                        };
+
+                        const item = {
+                            hidden: {
+                                opacity: 0,
+                                y: 20,
+                            },
+                            show: {
+                                opacity: 1,
+                                y: 0,
+                            }
+                        };
+
+                        return (
+                            mesDemandes && (
+                                mesDemandes.length > 0 ? (
+                                    <motion.div
+                                        className="flex grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-32 mt-32 sm:mt-40"
+                                        variants={container}
+                                        initial="hidden"
+                                        animate="show"
+                                    >
+                                        {mesDemandes.map((demande) => {
+                                            return (
+                                                <motion.div variants={item} key={demande.id}>
+
+                                                </motion.div>
+                                            )
+                                        })}
+                                    </motion.div>
+                                ) : (
+                                    <div className="flex flex-1 items-center justify-center">
+                                        <Typography color="text.secondary" className="text-24 my-24">
+                                            Aucune demande trouvé !
+                                        </Typography>
+                                    </div>
+                                )
+                            )
+                        )
+                    }, [mesDemandes])}
                 </div>
             }
         />
