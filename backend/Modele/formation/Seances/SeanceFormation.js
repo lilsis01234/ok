fa5845b64c94286a8de23a923db77ab85faf46fe
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2024-06-22 20:04:08 +0300

    ajout séance backend

--- backend/Modele/formation/Seances/SeanceFormation.js
+++ backend/Modele/formation/Seances/SeanceFormation.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require('../../../database/database');
-const Module = require('../Modules/Module')
+const Module = require('../Modules/Module');
+const Formation = require('../Formation');
 
 class SeanceFormation extends Model { }
 
@@ -20,25 +21,28 @@ SeanceFormation.init({
         type: DataTypes.INTEGER,
         allowNull: true,
         references: {
-            model : Module,
+            model: Module,
             key: 'id',
         },
     },
-    nombreDePlaces : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
+    nombreDePlaces: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
-    nombredePlacesReservees : {
-        type : DataTypes.INTEGER,
-        allowNull : false
+    nombredePlacesReservees: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }, 
-    title : {
-        type : DataTypes.STRING,
-        allowNull : false,
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
     }
 }, {
     sequelize,
-    modelName : 'Formation_Seance'
-})
+    modelName: 'Formation_Seance'
+});
+
+SeanceFormation.belongsTo(Module, { foreignKey: 'module' });
+SeanceFormation.belongsTo(Formation, { foreignKey: 'formation' });
 
 module.exports = SeanceFormation;
